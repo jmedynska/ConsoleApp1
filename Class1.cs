1c60b240f558d2e3fa3c5554c30f5ae98bc36820
Julia Medynska <julia.medynska1@gmail.com>
2022-02-19 16:52:06 +0100

    Added new class

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args) { 
+        
+       // Code to call Feature1
+                }
+    }
+}
